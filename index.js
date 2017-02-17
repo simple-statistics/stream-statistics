4f72c719f78cac4d2e42f260f1187f100fb79ed1
Tom MacWright <tom@macwright.org>
2017-02-17 18:17:07 -0500

    Simplify

--- index.js
+++ index.js
@@ -1,202 +1,21 @@
-// var through2 = require('through2');
-
-/**
- * Create a sum function
- */
-function createSum() {
-  var sum = 0;
-  return function addItem(item) {
-    if (item == undefined) {
-      return sum;
-    }
-    return sum += item;
-  };
+function introduce(fn, memo, dependents) {
+  return (function(memo) {
+    dependents = dependents || [];
+    return function(item) {
+      return item === undefined ? memo : memo = fn.apply(undefined, [item, memo].concat(dependents.map(d => d(item))));
+    };
+  }).bind(undefined, memo);
 }
-module.exports.createSum = createSum;
 
-/**
- * Create a mean function
- */
-function createCount() {
-  var count = 0;
-  return function addItem(item) {
-    if (item == undefined) {
-      return count;
-    }
-    return count += 1;
-  };
-}
-module.exports.createCount = createCount;
+module.exports.createSum = introduce((item, sum) => sum + item, 0);
 
-/**
- * Create a mean function
- */
-function createMean() {
-  var count = createCount();
-  var sum = createSum();
-  return function addItem(item) {
-    if (item == undefined) {
-      return sum() / count();
-    }
-    return sum(item) / count(item);
-  };
-}
-module.exports.createMean = createMean;
+module.exports.createCount = introduce((item, count) => ++count, 0);
 
-function createGeometricMean() {
-  var count = createCount();
-  var rollingMean = 1;
-  return function addItem(item) {
-    if (item == undefined || item < 0) {
-      return Math.pow(rollingMean, 1 / count());
-    }
-    return Math.pow(rollingMean *= item, 1 / count(item));
-  };
-}
-module.exports.createGeometricMean = createGeometricMean;
+module.exports.createMean = introduce((item, last, sum, count) => sum / count, undefined, [
+  module.exports.createSum(),
+  module.exports.createCount()
+]);
 
-function createMin() {
-  var min;
-  return function addItem(item) {
-    if (item === undefined) {
-      return min;
-    } else if (min === undefined || item < min) {
-      min = item;
-    }
-    return min;
-  };
-}
-module.exports.createMin = createMin;
+module.exports.createMin = introduce((item, min) => min === undefined || item < min ? item : min, undefined);
 
-function createMax() {
-  var max;
-  return function addItem(item) {
-    if (item === undefined) {
-      return max;
-    } else if (max === undefined || item > max) {
-      max = item;
-    }
-    return max;
-  };
-}
-module.exports.createMax = createMax;
-
-// TODO: not right yet
-// function createSumOfSquares() {
-//   var count = createCount();
-//   var mean = createMean();
-//   var sumOfSquares = 0;
-//   return function addItem(item) {
-//     var c = count(item);
-//     return sumOfSquares += (c - 1) * Math.pow(item - mean(item), 2) / c;
-//   };
-// }
-// module.exports.createSumOfSquares = createSumOfSquares;
-// function createVariance() {
-//   var count = createCount();
-//   var sumOfSquares = createSumOfSquares();
-//   return function addItem(item) {
-//     return sumOfSquares(item) / count(item);
-//   };
-// }
-// module.exports.createVariance = createVariance;
-
-/*
-module.exports = function() {
-    return through2({
-        objectMode: true
-    }, function(chunk, enc, callback) {
-        if (this._stats === undefined) {
-            this._stats = {
-                max: null,
-                min: null,
-                n: 0,
-                _geometric_mean: 1,
-                _reciprocal_sum: 0,
-                mean: null,
-                ss: null,
-                sum: null,
-                // for mode calculations
-                _seen_this: null,
-                _mode: null,
-                _mode_valid: true,
-                get variance() {
-                    return this.ss / this.n;
-                },
-                get standardDeviation() {
-                    return Math.sqrt(this.variance);
-                },
-                get geometricMean() {
-                    return Math.pow(this._geometric_mean, 1 / this.n);
-                },
-                get harmonicMean() {
-                    return this.n / this._reciprocal_sum;
-                },
-                get mode() {
-                    if (this._mode_valid) {
-                        if (this._seen_this > this._max_seen) {
-                            return this._last;
-                        } else {
-                            return this._mode;
-                        }
-                    }
-                }
-            };
-        }
-
-        var x = parseFloat(chunk);
-
-        if (this._stats.n === 0) {
-            this._stats.min = x;
-            this._stats.max = x;
-            this._stats.mean = x;
-            this._stats.sum = x;
-
-            // mode calculations
-            // the current mode
-            this._stats._mode = x;
-            // seen the current value
-            this._stats._seen_this = 1;
-            // seen the mode
-            this._stats._max_seen = 1;
-            this._stats._last = x;
-        } else {
-            if (x < this._stats.min) this._stats.min = x;
-            if (x > this._stats.max) this._stats.max = x;
-            this._stats.ss += this._stats.n *
-                Math.pow(x - this._stats.mean, 2) /
-                (this._stats.n + 1);
-            this._stats.mean += (x - this._stats.mean) /
-                (this._stats.n + 1);
-            this._stats.sum += x;
-            if (this._stats._last < x) {
-                this._mode_valid = false;
-            }
-            if (this._stats._mode_valid) {
-                if (x !== this._stats._last) {
-                    if (this._stats._seen_this > this._stats._max_seen) {
-                        this._stats._max_seen = this._stats._seen_this;
-                        this._stats._seen_this = 1;
-                        this._stats._mode = this._stats._last;
-                    }
-                    this._stats._last = x;
-                } else {
-                    this._stats._seen_this++;
-                }
-            }
-        }
-
-        // geometric mean is only valid for positive numbers
-        if (x > 0) {
-            this._stats._geometric_mean *= x;
-            this._stats._reciprocal_sum += 1 / x;
-        }
-
-        this._stats.n++;
-        callback();
-    }, function(callback) {
-        this.push(this._stats);
-        callback();
-    });
-};
-*/
+module.exports.createMax = introduce((item, max) => max === undefined || item > max ? item : max, undefined);
--- test/stream_statistics.test.js
+++ test/stream_statistics.test.js
@@ -1,58 +1,17 @@
 var test = require('tap').test;
 var StreamStatistics = require('../');
 
-var testData = [
-  {
-    val: 1,
-    count: 1,
-    mean: 1,
-    min: 1,
-    max: 1,
-    sum: 1,
-    geometricMean: 1
-  },
-  {
-    val: 2,
-    count: 2,
-    mean: 1.5,
-    min: 1,
-    max: 2,
-    sum: 3,
-    geometricMean: 1
-  },
-  {
-    val: 3,
-    count: 3,
-    mean: 2,
-    min: 1,
-    max: 3,
-    sum: 6,
-    geometricMean: 1
-  },
-  {
-    val: -6,
-    count: 4,
-    mean: 0,
-    min: -6,
-    max: 3,
-    sum: 0,
-    geometricMean: 1
-  }
-];
-
 test('StreamStatistics', function(t) {
-  var count = StreamStatistics.createCount();
-  var mean = StreamStatistics.createMean();
-  var min = StreamStatistics.createMin();
-  var max = StreamStatistics.createMax();
-  var sum = StreamStatistics.createSum();
-  var geometricMean = StreamStatistics.createGeometricMean();
-  testData.forEach(function(input) {
-    t.equal(count(input.val), input.count, 'count');
-    t.equal(mean(input.val), input.mean, 'mean');
-    t.equal(min(input.val), input.min, 'min');
-    t.equal(sum(input.val), input.sum, 'sum');
-    t.equal(geometricMean(input.val), input.geometricMean, 'geometricMean');
+  var input = [1, 2, 3, -6];
+  [
+    ['createCount', [1, 2, 3, 4]],
+    ['createMean', [1, 1.5, 2, 0]],
+    ['createMin', [1, 1, 1, -6]],
+    ['createMax', [1, 2, 3, 3]],
+    ['createSum', [1, 3, 6, 0]]
+  ].forEach(function(metric) {
+    var fn = StreamStatistics[metric[0]]();
+    t.deepEqual(input.map(fn), metric[1], metric);
   });
   t.end();
 });
